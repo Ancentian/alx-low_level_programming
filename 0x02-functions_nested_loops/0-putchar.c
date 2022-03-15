44f924ab06e8f8e0856a5b5dd4be62353404eef1
Ancentian <ancentmbithi8@gmail.com>
2022-03-15 06:58:56 -0700

    Functions and Nested loops task 0 to 2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c[]= "_putchar";
+int i;
+for(i=0;i<8;i++)
+{
+_putchar(c[8]);
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void);
+{
+int i;
+char c;
+for(i=10;i<10;++i);
+{
+for(c = 'a'; c<= 'z'; c++)
+}
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
