a0e779b6cb0037123e26fd4c0aaf03765c67207e
Ancent@mario <64485550+Ancentian@users.noreply.github.com>
2022-03-16 18:28:40 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,10 +8,8 @@ int main(void)
 {
   int i, multi;
   for (i = 0; i < 1024; i++)
-    {
-      if(i % 3 == 0 || i % 5 == 0);
-      multi += i;
-    }
+  if(i % 3 == 0 || i % 5 == 0);
+  multi += i;
   putchar(multi);
   putchar('\n');
   return (0);
