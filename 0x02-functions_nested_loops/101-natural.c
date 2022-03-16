9d9e0705fccebd135ccb489031c96e9db2989587
Ancentian <ancentmbithi8@gmail.com>
2022-03-16 08:17:48 -0700

    Advanced Functions

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  int i, multi;
+  for (i = 0; i < 1024; i++)
+    {
+      if(i % 3 == 0 || i % 5 == 0);
+      multi += i;
+    }
+  putchar(multi);
+  putchar('\n');
+  return (0);
+}
